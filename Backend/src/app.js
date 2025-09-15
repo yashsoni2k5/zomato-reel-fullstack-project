7bba3119618cff4919d6ec54a8a5371ade12e1b9
yash soni <yashsoni2k5@gmail.com>
2025-09-15 12:58:13 -0700

    changed cors origin to frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://zomato-reel-project.onrender.com",
     credentials:true
 }));
 app.use(cookieParser());
